d61aadbc86781f26d493ebc7c0d9fe20c6ccdf5f
Sulabh <sulabh4@hotmail.com>
2017-09-10 22:41:59 +0545

    Initial Commit

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTargetMover : MonoBehaviour {
+
+	public float spinSpeed = 180.0f;
+	public float motionMagnitude = 0.1f;
+	public bool doSpin = true;
+	public bool doMotion = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (doSpin) {
+			//Rotate around the up axis of the game object
+			gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime); //multlipied by time for more framerate
+		}
+
+		if (doMotion){
+		//Move up and down over time
+		gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);	//Math.Cos for wave pattern
+		}
+	}
+}
